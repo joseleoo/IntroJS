dde2827c291369b2745825acdca5374e79bcac77
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2025-01-15 13:35:13 -0500

    Initial commit

---
+++ Script.js
@@ -0,0 +1,15 @@
+let peliculaTitulo = "avengers" //global scope
+const peliculaPrecio=20
+function setTitulo() {
+    let peliculaTitulo = "Terminator 2" //second scope
+    console.log(peliculaTitulo)//second scope
+}
+function setPrecio() {
+    const peliculaPrecio = 50
+    console.log(peliculaPrecio)
+    peliculaPrecio = 60 //unable to change
+}
+setTitulo();
+setPrecio();
+console.log(peliculaTitulo)//global scope
+console.log(peliculaTitulo)
